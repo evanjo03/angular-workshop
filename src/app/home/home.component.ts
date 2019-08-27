a561715865823135e0c55ed6828a370b20b0af4d
John Evans <john.wes.evans@gmail.com>
2019-08-27 10:58:18 -0500

    some playground changes

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  clickCounter: number = 0;
+  name: string = "hey";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  countClick() {
+    this.clickCounter ++;
+  }
+
+  submit() {
+    console.log(this.name)
+  }
+
+}
